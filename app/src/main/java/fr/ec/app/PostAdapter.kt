9bbf34054556d35de5eb3d9e83c5e2610d61ac74
Mohammed Boukadir <mohammed@Mohammeds-MacBook-Pro-2.local>
2024-05-27 14:03:00 +0200

    Use getItemViewType to display different items

--- app/src/main/java/fr/ec/app/PostAdapter.kt
+++ app/src/main/java/fr/ec/app/PostAdapter.kt
@@ -9,16 +9,29 @@ import androidx.recyclerview.widget.RecyclerView.ViewHolder
 class PostAdapter(private val dataSource: List<Post>) : RecyclerView.Adapter<PostViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
-        return PostViewHolder(
-            itemView = LayoutInflater.from(parent.context)
-                .inflate(R.layout.post_item, parent, false)
-        ).also {
-            Log.d("PostAdapter","onCreateViewHolder $it")
+       return if(viewType == R.layout.post_item_big ) {
+            PostViewHolder(
+                itemView = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.post_item_big, parent, false)
+            )
+        } else {
+            PostViewHolder(
+                itemView = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.post_item, parent, false)
+            )
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) {
+            R.layout.post_item_big
+        } else {
+            R.layout.post_item
         }
     }
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {
-        Log.d("PostAdapter","onBindViewHolder position:$position $holder")
+        Log.d("PostAdapter", "onBindViewHolder position:$position $holder")
         holder.bind(dataSource[position])
     }
 
